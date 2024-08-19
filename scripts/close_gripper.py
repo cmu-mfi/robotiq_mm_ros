89ca7e9bd2a24cfaf003eaddba247ae2931a414c
Kevin Zhang <kevinleezhang@gmail.com>
2024-08-19 12:23:35 -0400

    Added open and close gripper files.

---
+++ scripts/close_gripper.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import rospy
+
+import actionlib
+
+from robotiq_mm_ros.msg import *
+
+def robotiq_client():
+    client = actionlib.SimpleActionClient('/yk_builder/move_gripper', GripperCommandAction)
+
+    client.wait_for_server()
+
+    goal = GripperCommandGoal(position=0.0, velocity = 0.1, force=165)
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  
+
+if __name__ == '__main__':
+    rospy.init_node('robotiq_client')
+    robotiq_client()
+
